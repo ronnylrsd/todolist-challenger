b46f6b9cf8094b7a93a0abeccfdfe5d48aee662e
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-07-17 09:45:29 -0300

    Feat: create e list do todo feito

---
+++ src/main/java/br/com/ronnyribeiro/todolistchallenger/service/TodoService.java
@@ -0,0 +1,39 @@
+package br.com.ronnyribeiro.todolistchallenger.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import br.com.ronnyribeiro.todolistchallenger.entity.Todo;
+import br.com.ronnyribeiro.todolistchallenger.repository.TodoRepository;
+
+@Service
+public class TodoService {
+    private TodoRepository todoRepository;
+    
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list(){
+        Sort sort = Sort.by("priority").descending().and(
+            Sort.by("name").ascending()
+        );
+        return todoRepository.findAll(sort);
+    }
+
+
+    public List<Todo> update(){
+        return list();
+    }
+
+    public List<Todo> delete(){
+        return list();
+    }
+}
