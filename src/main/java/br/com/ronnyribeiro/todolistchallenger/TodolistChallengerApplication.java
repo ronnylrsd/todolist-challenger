2772f9a947843495c44b201fbc2cf9aa800010ca
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-07-17 09:27:07 -0300

    Feat: create spring boot project

---
+++ src/main/java/br/com/ronnyribeiro/todolistchallenger/TodolistChallengerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ronnyribeiro.todolistchallenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistChallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistChallengerApplication.class, args);
+	}
+
+}
