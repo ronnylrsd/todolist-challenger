cd274c4dabdaca0efe30681ddd5c2f0d6c14703b
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-07-17 09:53:43 -0300

    Feat: adicionando endpoints

--- src/main/java/br/com/ronnyribeiro/todolistchallenger/controller/TodoController.java
+++ src/main/java/br/com/ronnyribeiro/todolistchallenger/controller/TodoController.java
@@ -2,6 +2,12 @@ package br.com.ronnyribeiro.todolistchallenger.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,20 +23,23 @@ public class TodoController {
         this.todoService = todoService;
     }
 
-    List<Todo> create (Todo todo){
+    @PostMapping
+    List<Todo> create (@RequestBody Todo todo){
         return todoService.create(todo);
     }
 
+    @GetMapping
     List<Todo> list(){
         return todoService.list();
     }
 
-
-    List<Todo> update(Todo todo){
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo){
         return todoService.update(todo);
     }
 
-    List<Todo> delete(Long id){
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable Long id){
         return todoService.delete(id);
     }
 }
