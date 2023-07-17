b250450d2fe4731a6c8455bb57b7ad0091968c90
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-07-17 09:31:34 -0300

    Feat: criada entidade todo

---
+++ src/main/java/br/com/ronnyribeiro/todolistchallenger/entity/Todo.java
@@ -0,0 +1,50 @@
+package br.com.ronnyribeiro.todolistchallenger.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean done;
+    private Integer priority;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Boolean getDone() {
+        return done;
+    }
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+    public Integer getPriority() {
+        return priority;
+    }
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+}
